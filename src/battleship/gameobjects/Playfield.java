80e6c9a1861b9ce9f93c4ea3420a0b52c08bd168
Tyscorp <Tyscorp@e3f73532-79e5-11de-9499-4f8beaa274e3>
2009-07-27 19:59:32 +0000

    Added basic 'Playfield' - for drawing battleships etc to.

--- src/battleship/gameobjects/Playfield.java
+++ src/battleship/gameobjects/Playfield.java
@@ -3,114 +3,65 @@ package battleship.gameobjects;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Point;
 import java.awt.Rectangle;
-import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 import battleship.EventManager;
 
 public class Playfield extends GameObject
 {
-	private Dimension gridSize;
-	private int[][] grid;
-	private Point xy = new Point(-1, -1);
-	private MouseEvent mouse;
-	private boolean mousePressed = false;
+	public Dimension gridSize;
+	public List<GameObject> obj = new ArrayList<GameObject>();
 	
-	public Playfield(Point p, Dimension d, Dimension g)
+	public Playfield(Rectangle r, Dimension d)
 	{
-		super(new Rectangle(p.x, p.y, d.width*g.width+1, d.height*g.height+1));
-		gridSize = g;
-		grid = new int[d.width][d.height];
-		
-		//lol
-		grid[1][1] = 1;
-		grid[2][1] = 1;
-		grid[3][1] = 1;
-		grid[2][2] = 1;
-		grid[2][3] = 1;
-		grid[1][4] = 1;
-		grid[2][4] = 1;
-		grid[3][4] = 1;
-		grid[5][1] = 1;
-		grid[5][2] = 1;
-		grid[5][3] = 1;
-		grid[5][4] = 1;
-		grid[6][2] = 1;
-		grid[7][3] = 1;
-		grid[8][1] = 1;
-		grid[8][2] = 1;
-		grid[8][3] = 1;
-		grid[8][4] = 1;
-		grid[10][1] = 1;
-		grid[11][1] = 1;
-		grid[12][1] = 1;
-		grid[11][2] = 1;
-		grid[11][3] = 1;
-		grid[11][4] = 1;
-		grid[14][1] = 1;
-		grid[14][2] = 1;
-		grid[14][3] = 1;
-		grid[14][4] = 1;
-		grid[15][1] = 1;
-		grid[15][3] = 1;
-		grid[16][1] = 1;
-		grid[16][2] = 1;
-		grid[16][3] = 1;
-		
+		super(new Rectangle(r.x, r.y, r.width*d.width+1, r.height*d.height+1));
+		gridSize = d;
 		render();
 	}
 
-
+	public Playfield(String s, Rectangle r, Dimension d)
+	{
+		super(new Rectangle(r.x, r.y, r.width*d.width+1, r.height*d.height+1));
+		gridSize = d;
+		name = s;
+		render();
+	}
+	
 	@Override
 	public void update()
 	{
-		if(mousePressed && xy.x >= 0 && xy.x < r.width/gridSize.width && xy.y >= 0 && xy.y < r.width/gridSize.height)
+		for(GameObject go : obj)
 		{
-			if(mouse.getButton() == MouseEvent.BUTTON1)
-				grid[xy.x][xy.y]++;
-			else if(mouse.getButton() == MouseEvent.BUTTON3)
-				grid[xy.x][xy.y]--;
-			render();
+			go.update();
 		}
 	}
 	
 	@Override
 	public void render()
 	{
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, r.width, r.height);
+		//draw grid
 		g.setColor(Color.BLACK);
-		for(int i = 0; i < r.width/gridSize.width+1; i++)
+		for(int i = 0; i <= r.width/gridSize.width; i++)
 		{
 			g.drawLine(i*gridSize.width, 0, i*gridSize.width, r.height);
 		}
-		for(int i = 0; i < r.height/gridSize.height+1; i++)
+		for(int i = 0; i <= r.height/gridSize.height; i++)
 		{
 			g.drawLine(0, i*gridSize.height, r.width, i*gridSize.height);
 		}
-		for(int i = 0; i < r.width/gridSize.width; i++)
+		
+		//draw battleships
+		for(GameObject go : obj)
 		{
-			for(int j = 0; j < r.height/gridSize.height; j++)
-			{
-				if(grid[i][j] > 0)
-				{
-					g.setColor(Color.PINK);
-					g.fillRect(i*gridSize.width+1, j*gridSize.height+1, gridSize.width-1, gridSize.height-1);
-					g.setColor(Color.BLACK);
-					g.drawString(""+grid[i][j], i*gridSize.width+1, j*gridSize.height+10);
-				}
-			}
+			go.render();
 		}
-		if(xy.x >= 0)
+		
+		//draw battleships to grid
+		for(GameObject go : obj)
 		{
-			g.setColor(new Color(255, 0, 0, 128));
-			g.fillRect(xy.x*gridSize.width, 0, gridSize.width, r.height);
-		}
-		if(xy.y >= 0)
-		{
-			g.setColor(new Color(0, 0, 255, 128));
-			g.fillRect(0, xy.y*gridSize.height, r.width, gridSize.height);
+			go.paint(g);
 		}
 	}
 	
@@ -129,39 +80,6 @@ public class Playfield extends GameObject
 	@Override
 	public void pumpEvents(EventManager em)
 	{
-		for(int i = 0; i < em.size(); i++)
-		{
-			if(em.get(i).event.startsWith("mouse"))
-			{
-				MouseEvent me = (MouseEvent) em.get(i).param;
-				if(em.get(i).event.equals("mouseMoved") || em.get(i).event.equals("mouseDragged"))
-				{
-					if(r.contains(me.getPoint()))
-					{
-						xy.x = me.getX()/gridSize.width - r.x/gridSize.width;
-						xy.y = me.getY()/gridSize.height - r.y/gridSize.height;
-					}
-					else
-					{
-						xy.x = -1;
-						xy.y = -1;
-					}
-					render();
-				}
-				else if(em.get(i).event.equals("mousePressed"))
-				{
-					mousePressed = true;
-					mouse = me;
-				}
-				else if(em.get(i).event.equals("mouseReleased"))
-				{
-					mousePressed = false;
-				}
-				else if(em.get(i).event.equals("mouseClicked"))
-				{
-					
-				}
-			}
-		}
+		
 	}
 }
--- src/battleship/states/GameState.java
+++ src/battleship/states/GameState.java
@@ -1,19 +1,15 @@
 package battleship.states;
 
-import java.awt.Color;
+
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Point;
 import java.awt.Rectangle;
-import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
 
 import battleship.Event;
 import battleship.EventManager;
-import battleship.gameobjects.Button;
-import battleship.gameobjects.GameObject;
-import battleship.gameobjects.Playfield;
+import battleship.gameobjects.*;
 
 public class GameState extends State
 {
@@ -22,31 +18,11 @@ public class GameState extends State
 	public GameState()
 	{
 		name = "GameState";
-		
-		//example of creating a button at run-time
-		BufferedImage img1 = new BufferedImage(192, 128, BufferedImage.TRANSLUCENT);
-		BufferedImage img2 = new BufferedImage(192, 128, BufferedImage.TRANSLUCENT);
-		BufferedImage img3 = new BufferedImage(192, 128, BufferedImage.TRANSLUCENT);
-		Graphics g = img1.getGraphics();
-		g.setColor(Color.GRAY);
-		g.fillRect(0, 0, img1.getWidth(), img1.getHeight());
-		g.setColor(Color.BLACK);
-		g.drawRect(0, 0, img1.getWidth()-1, img1.getHeight()-1);
-		g.drawString("LOL", 75, 50);
-		g = img2.getGraphics();
-		g.setColor(Color.ORANGE);
-		g.fillRect(0, 0, img2.getWidth(), img2.getHeight());
-		g.setColor(Color.BLACK);
-		g.drawRect(0, 0, img2.getWidth()-1, img2.getHeight()-1);
-		g.drawString("TEST", 75, 50);
-		g = img3.getGraphics();
-		g.setColor(Color.MAGENTA);
-		g.fillRect(0, 0, img3.getWidth(), img3.getHeight());
-		g.setColor(Color.BLACK);
-		g.drawRect(0, 0, img3.getWidth()-1, img3.getHeight()-1);
-		g.drawString("BUTTON", 75, 50);
-		obj.add(new Button(new Rectangle(208, 408, 192, 128), "TestButton", img1, img2, img3));
-		obj.add(new Playfield(new Point(128, 0), new Dimension(20, 20), new Dimension(16, 16)));
+		Playfield p = new Playfield("Status Screen", new Rectangle(24, 0, 10, 10), new Dimension(24, 24));
+		p.obj.add(new Battleship(new Rectangle(8*p.gridSize.width, 4*p.gridSize.height, 1*p.gridSize.width, 2*p.gridSize.height)));
+		p.render();
+		obj.add(p);
+		obj.add(new Playfield("Targeting Screen", new Rectangle(24, 240+24, 10, 10), new Dimension(24, 24)));
 	}
 
 	@Override
@@ -105,13 +81,15 @@ public class GameState extends State
 				if(em.get(i).param.equals("Host"))
 				{
 					//insert host specific stuff here
-					
+					sem.add(new Event("setState", "MenuState"));
+					sem.add(new Event("error", "Hosting not implemented"));
 					em.consume(i);
 				}
 				else if(em.get(i).param.equals("Join"))
 				{
 					//insert join specific stuff here
-					
+					sem.add(new Event("setState", "MenuState"));
+					sem.add(new Event("error", "Joining not implemented"));
 					em.consume(i);
 				}
 				else if(em.get(i).param.equals("Single"))
