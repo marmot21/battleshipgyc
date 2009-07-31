29ef216af2cf76f0fb1120f02ba2e84a9bd5c9b1
Marmot.Daniel <Marmot.Daniel@e3f73532-79e5-11de-9499-4f8beaa274e3>
2009-07-31 14:08:22 +0000

    Work on drag and drop stuff

--- trunk/src/battleship/gameobjects/Battleship.java
+++ trunk/src/battleship/gameobjects/Battleship.java
@@ -3,6 +3,7 @@ package battleship.gameobjects;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 import battleship.EventManager;
 
@@ -12,25 +13,36 @@ public class Battleship extends GameObject
 		NORMAL, FOLLOW, INIT
 	}
 	public SHIPS STATE = SHIPS.NORMAL;
-	
-	public GameImage Image;
+	protected GameImage Image;
+	protected static Rectangle statusScreen = new Rectangle(24, 240+24, 241+24, 241+240+24);
+	protected static Position prevPos = new Position();
+	protected static ArrayList<Battleship> inits = new ArrayList<Battleship>();
 	
 	public Battleship(GameImage i)
 	{
 		super(new Rectangle(0, 0, 1, 1));
 		Image = i;
+		r.x = inits.size()*30+400;
+		r.y = ((inits.size()-inits.size()%2)/2)*26;
+		inits.add(this);
 	}
 	
 	public Battleship(Rectangle r, GameImage i)
 	{
 		super(r);
 		Image = i;
+		r.x = inits.size()*30+400;
+		r.y = (inits.size()-inits.size()%2/2)*26;
+		inits.add(this);
 	}
 	
 	public Battleship(Rectangle r, String s, GameImage i)
 	{
 		super(r, s);
 		Image = i;
+		r.x = inits.size()*30+400;
+		r.y = (inits.size()-inits.size()%2/2)*26;
+		inits.add(this);
 	}
 
 
@@ -72,23 +84,35 @@ public class Battleship extends GameObject
 					if(STATE == SHIPS.FOLLOW) {
 						r.x = me.getX()-r.width/2;
 						r.y = me.getY();//-r.height/2;
-						resize();
-					}
-					if(r.contains(me.getPoint()) && STATE == SHIPS.NORMAL){
-						STATE = SHIPS.FOLLOW;
 					}
 					
 				}
 				else if(em.get(i).event.equals("mouseReleased")) {
-					if(STATE == SHIPS.FOLLOW){
+					if(STATE == SHIPS.FOLLOW && statusScreen.contains(me.getPoint())) {
 						STATE = SHIPS.NORMAL;
 						//snap it to the grid
-						r.y = r.y/24*24 + 4;
+						r.y = r.y/24*24;
 						r.x = r.x/24*24;
+						if(prevPos.STATE == SHIPS.INIT) {
+							inits.remove(this);
+						}
+					}
+					else if(STATE == SHIPS.FOLLOW && !statusScreen.contains(me.getPoint())) {
+						r.x = prevPos.x;
+						r.y = prevPos.y;
+						STATE = prevPos.STATE;
+					}
+					else if(STATE == SHIPS.NORMAL) {
+						prevPos.x = r.x;
+						prevPos.y = r.y;
+						prevPos.STATE = STATE;
 					}
 				}
 				else if(em.get(i).event.equals("mousePressed")) {
-					if(STATE == SHIPS.NORMAL && r.contains(me.getPoint())) {
+					if(STATE == SHIPS.INIT || STATE == SHIPS.NORMAL && r.contains(me.getPoint())) {
+						prevPos.x = r.x;
+						prevPos.y = r.y;
+						prevPos.STATE = STATE;
 						STATE = SHIPS.FOLLOW;
 					}
 				}
